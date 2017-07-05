cafc1db98054f49aa16b071d6f3b4495ad35a8c3
mlga <mlga@go-otserv.org>
2017-07-06 00:35:24 +0200

    Print information about using build cache

--- .ci-scripts/install-caf.sh
+++ .ci-scripts/install-caf.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 if [ -d $HOME/.cache/actor-framework ]; then
+    echo "Using CAF from cache"
     cd $HOME/.cache/actor-framework
     make install
 else
+    echo "Building CAF from source"
     cd $HOME/.cache
     git clone https://github.com/actor-framework/actor-framework.git
     cd actor-framework
--- .ci-scripts/install-cmake.sh
+++ .ci-scripts/install-cmake.sh
@@ -3,9 +3,11 @@ apt-get remove cmake
 apt-get purge cmake
 
 if [ -d $HOME/.cache/cmake-3.8.2 ]; then
+    echo "Using cmake from cache"
     cd $HOME/.cache/cmake-3.8.2
     make install
 else
+    echo "Building cmake from source"
     cd $HOME/.cache
     wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
     tar -zxf cmake-3.8.2.tar.gz

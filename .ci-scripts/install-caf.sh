32c32441aeb4e786234ed454e407f6debf361cf5
mlga <mlga@go-otserv.org>
2017-07-06 01:00:55 +0200

    Remove debug ls command

--- .ci-scripts/install-caf.sh
+++ .ci-scripts/install-caf.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 if [ -d $HOME/.cache/actor-framework ]; then
-    echo -e "\n########################################"
-    echo "# Using CAF from cache                 #"
-    echo -e "########################################\n"
+    echo
+    echo
+    echo "Using CAF from cache."
+    echo
+    echo
     cd $HOME/.cache/actor-framework
     make install
 else
-    echo -e "\n########################################"
-    echo "# Building CAF from source             #"
-    echo -e "########################################\n"
+    echo
+    echo
+    echo "Building CAF from source."
+    echo
+    echo
     cd $HOME/.cache
     git clone https://github.com/actor-framework/actor-framework.git
     cd actor-framework
--- .ci-scripts/install-cmake.sh
+++ .ci-scripts/install-cmake.sh
@@ -2,19 +2,20 @@
 apt-get remove cmake
 apt-get purge cmake
 
-ls -al $HOME
-ls -al $HOME/.cache
-
 if [ -d $HOME/.cache/cmake-3.8.2 ]; then
-    echo -e "\n########################################"
-    echo "# Using cmake from cache               #"
-    echo -e "########################################\n"
+    echo
+    echo
+    echo "Using cmake from cache."
+    echo
+    echo
     cd $HOME/.cache/cmake-3.8.2
     make install
 else
-    echo -e "\n########################################"
-    echo "# Building cmake from source           #"
-    echo -e "########################################\n"
+    echo
+    echo
+    echo "Building cmake from source."
+    echo
+    echo
     cd $HOME/.cache
     wget https://cmake.org/files/v3.8/cmake-3.8.2.tar.gz
     tar -zxf cmake-3.8.2.tar.gz
